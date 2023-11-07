770519904a5aa6ed61eae10758ae76f59c717dbe
Sumaiya-Meem <sumaiyameem.cse6.bu@gmail.com>
2023-11-07 12:05:01 +0600

    show modal with food info

--- src/Pages/SingleFood/SingleFood.jsx
+++ src/Pages/SingleFood/SingleFood.jsx
@@ -1,48 +1,123 @@
 import { useLoaderData } from "react-router-dom";
-import { Avatar } from 'flowbite-react';
-import { Card } from 'flowbite-react';
-// import Image from 'next/image';
+import { Card, Label, TextInput } from 'flowbite-react';
+import { Button, Modal } from 'flowbite-react';
+import { HiOutlineArrowRight } from 'react-icons/hi';
+import { useState } from 'react';
 
 const SingleFood = () => {
     const data = useLoaderData()
     // console.log(data)
-    const { userImage, userName,foodImage,foodName ,location} = data || "Not-Given"
+
+    const [openModal, setOpenModal] = useState(false);
+    const [customerNote, setCustomerNote] = useState('');
+    const [donation, setDonation] = useState('');
+
+    const { _id, userName, userEmail, foodImage, foodName, location, quantity, date } = data || "Not-Given"
     // console.log(userName,userImage)
+    const getCurrentTime = () => {
+        const currentTime = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+        return currentTime;
+    };
+
+    const currentTime = getCurrentTime(); 
+
+
     return (
-        <div className="max-w-[700px] mx-auto border-2 border-red-500">
+        <div className="max-w-[700px] mx-auto mt-10 p-2">
 
-            <div className="flex gap-2  shadow-lg">
-                <div className="flex-1 flex flex-col items-center justify-center border-2 border-red-400 p-2">
-                    <h1 className="font-semibold text-center mb-4 text-[#3e9785] text-2xl">Donor Information</h1>
-                    
-                        <div className="space-y-1 font-medium ">
-                            <div className=''> 
+            <div className="flex flex-col md:flex-row shadow-lg">
+                <div className="flex-1 flex flex-col items-center justify-center border-2  p-2">
+                    <h1 className="font-semibold text-center mb-4 text-[#6456d2] text-2xl">Donor Information</h1>
+
+                    <div className="space-y-1 font-medium ">
+                        <div className='text-black'>
                             <p>Donor Name:
-                            <span className='ml-2 capitalize text-[#6f60e2]'>{userName}</span></p>
-                            </div>
-                            <div className=''> 
-                                 <p>Pickup Location:
-                                <span className='ml-2 capitalize text-[#6f60e2]'>{location}</span></p>
-                            </div>
+                                <span className='ml-2 capitalize text-[#6f60e2]'>{userName}</span></p>
                         </div>
+                        <div className='text-black'>
+                            <p>Pickup Location:
+                                <span className='ml-2 capitalize text-[#6f60e2]'>{location}</span></p>
+                        </div>
+                    </div>
                 </div>
-                <div className="flex-1 border-2 border-yellow-400">
-                    
+                <div className="flex-1 border-2">
 
-    <Card className="">
-        <img src={foodImage} alt="" className="h-[100px] rounded-lg"/>
-      <h5 className="text-xl font-bold  text-gray-900 ">
-        {foodName}
-      </h5>
-      <p className="font-normal text-gray-700 dark:text-gray-400">
-        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
-      </p>
-    </Card>
-  
 
+                    <Card className="border-0">
+                        <img src={foodImage} alt="" className="h-[100px] rounded-lg" />
+                        <h5 className="text-xl font-semibold  text-gray-900 ">
+                            Food Item: {foodName}
+                        </h5>
+                        <p className="text-lg   text-gray-900">Quantity: {quantity}</p>
+                        <p className="text-lg   text-gray-900">Expired Date: {date}</p>
+                        <div>
+                            <Button outline pill onClick={() => setOpenModal(true)}>
+
+                                <p className="text-lg ">Request</p>
+                                <HiOutlineArrowRight className="ml-2 h-6 w-6" />
+                            </Button>
+                        </div>
+                    </Card>
                 </div>
 
             </div>
+
+            {/* Modal section*/}
+            <Modal dismissible show={openModal} onClose={() => setOpenModal(false)} >
+               
+                <Modal.Body>
+                    <div className="space-y-2">
+                    <div className="mb-2 flex items-center gap-2">
+                        <img src={foodImage} alt="" className="h-[150px] w-[70%] mx-auto"/>
+                </div>
+                <div className="mb-2 flex items-center gap-2">
+                        <Label value="Food No" className="flex-1" />
+                        <TextInput type="text" placeholder="Food Name" value={_id} disabled  className="flex-1" />
+                </div>
+                <div className="mb-2 flex items-center gap-2">
+                        <Label  value="Food Item" className="flex-1"/>
+                        <TextInput type="text" placeholder="Food Name" value={foodName} disabled className="flex-1"/>
+                </div>
+                <div className="mb-2 flex items-center gap-2">
+                        <Label  value="Donator Name" className="flex-1"/>
+                        <TextInput type="text" placeholder="Food Name" value={userName} disabled className="flex-1"/>
+                </div>
+                <div className="mb-2 flex items-center gap-2">
+                        <Label  value="Donator Email" className="flex-1"/>
+                        <TextInput type="text" placeholder="Food Name" value={userEmail} disabled className="flex-1"/>
+                </div>
+                <div className="mb-2 flex items-center gap-2">
+                        <Label  value="Donator Email" className="flex-1"/>
+                        <TextInput type="text" placeholder="Current Time" value={currentTime}  className="flex-1"/>
+                </div>
+
+                <div className="mb-2 flex items-center gap-2">
+                        <Label  value="Pickup Location" className="flex-1"/>
+                        <TextInput type="text" placeholder="Current Time" value={location} disabled  className="flex-1"/>
+                </div>
+                <div className="mb-2 flex items-center gap-2">
+                        <Label  value="Expired Date" className="flex-1"/>
+                        <TextInput type="text" placeholder="Current Time" value={date}  disabled   className="flex-1"/>
+                </div>
+                <div className="mb-2 flex items-center gap-2">
+                        <Label  value="Additional Notes" className="flex-1"/>
+                        <TextInput type="text" placeholder="Note" value={customerNote} onChange={(e)=>setCustomerNote(e.target.value)}  className="flex-1"/>
+                </div>
+                <div className="mb-2 flex items-center gap-2">
+                        <Label  value=" Donation Money" className="flex-1"/>
+                        <TextInput type="text" placeholder="$" value={donation} onChange={(e)=>setDonation(e.target.value)}  className="flex-1"/>
+                </div>
+                
+                </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={() => setOpenModal(false)}>Request</Button>
+                    <Button color="gray" onClick={() => setOpenModal(false)}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
 
 
         </div>
